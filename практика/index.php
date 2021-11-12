e3de23a52269d09763ff94a2bf051378b47b1b3c
lValerial <90343586+lValerial@users.noreply.github.com>
2021-11-12 22:22:35 +0400

    Add files via upload

---
+++ практика/index.php
@@ -0,0 +1,40 @@
+<?php
+?>
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="mem.css">
+    <title>Memory</title>
+</head>
+<body>
+<header>
+    <ul>
+        <li class="Verx" >
+            <div class="nazv">
+              <h2> Books </h2><br><h2> Memory</h2>
+
+            </div>
+            <div class="log">
+                <img src="img\logo.png">
+            </div>
+        </li>
+
+        <li class="poisk">
+            <p><input type="search" name="q" placeholder="Поиск по сайту">
+                <input type="submit" value="Найти"></p>
+        </li>
+        <li>
+            <button class="vxod" >Войти</button>
+        </li>
+    </ul>
+    <section class="slog">
+
+        <h1>
+            Хранилище знаний
+        </h1>
+
+
+    </section>
