0f6fc096070eb461104f3035e45d51aa5ee6f187
lValerial <90343586+lValerial@users.noreply.github.com>
2021-11-13 10:53:20 +0400

    Add files via upload

---
+++ практика/Proekt/index.php
@@ -0,0 +1,42 @@
+<?php
+?>
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="mem.css">
+    <title>Memory</title>
+</head>
+<body>
+<header>
+    <ul>
+        <li class="Verx" >
+            <div class="nazv">
+                <h2> Books </h2><br><h2> Memory</h2>
+
+            </div>
+            <div class="log">
+                <img src="img\logo.png">
+            </div>
+        </li>
+
+        <li class="poisk">
+
+
+        </li>
+        <li>
+           <button class="vxod" <a href="vendor/sign_up.php"> </a>Войти</button> <br>
+
+            <a href="registr/registr.php" class="reg">Зарегистрироваться</a>
+        </li>
+    </ul>
+    <section class="slog">
+
+        <h1>
+            Хранилище знаний
+        </h1>
+
+
+    </section>
